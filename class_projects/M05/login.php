663bbe4a3d1d022c3c18fa3ca9cce8ea38c95099
IBMSDEV <ibays@ivytech.edu>
2021-04-21 16:46:03 -0400

    M05 Discovery Project - Implementing the Database

---
+++ class_projects/M05/login.php
@@ -0,0 +1,16 @@
+<?php
+//Ian Bays
+
+ //require_once('bookmark_fns.php');
+ 
+ // ▼ this line is needed for the HTML to Load 
+ require_once('output_fns.php');
+
+
+ do_html_header('');
+
+ display_site_info();
+ display_login_form();
+
+ do_html_footer();
+?>
